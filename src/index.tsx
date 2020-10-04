5030801f9e2623ef3f9c551b66763a0c30e4e6b1
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-10-04 20:42:08 +0200

    chore: configure react-modal app-element

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
+import { setAppElement } from 'react-modal';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appRootElement = document.getElementById('root');
+
+if (appRootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    appRootElement
+  );
+
+  setAppElement(appRootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
